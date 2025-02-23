24e0660223ed2ef6c8b5bf94fb36195e0e160138
E.Laptev <E.Laptev@amtelsoft.ru>
2025-02-23 11:56:51 +0300

    3.7 Тернарное сравнение

---
+++ src/test/java/ru/condition/MaxTest.java
@@ -0,0 +1,43 @@
+package ru.condition;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class MaxTest {
+    @Test
+    void whenMax1To2Then2() {
+        int left = 1;
+        int right = 2;
+        int result = Max.max(left, right);
+        int expected = 2;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenMax3To2Then3() {
+        int left = 3;
+        int right = 2;
+        int result = Max.max(left, right);
+        int expected = 3;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenMax1To4Then4() {
+        int left = 1;
+        int right = 4;
+        int result = Max.max(left, right);
+        int expected = 4;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenMax1To1Then1() {
+        int left = 1;
+        int right = 1;
+        int result = Max.max(left, right);
+        int expected = 1;
+        assertThat(result).isEqualTo(expected);
+    }
+}
