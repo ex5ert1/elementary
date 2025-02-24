7d883ad6812bf22f43b5c53a56d00b7f2e4969b4
E.Laptev <E.Laptev@amtelsoft.ru>
2025-02-24 20:46:41 +0300

    3.16 Отладка программы в Idea

---
+++ src/main/java/ru/condition/ThreeMax.java
@@ -0,0 +1,17 @@
+package ru.condition;
+
+public class ThreeMax {
+    public static int max(int first, int second, int third) {
+        int result = first;
+
+        if (first >= second && first >= third) {
+            result = first;
+        } else if (second >= first && second >= third) {
+            result = second;
+        } else {
+            result = third;
+        }
+        return result;
+    }
+}
+
