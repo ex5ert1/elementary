02187c02f3944ac0d2c49924cbeb5374c973e62e
E.Laptev <E.Laptev@amtelsoft.ru>
2025-02-25 18:34:02 +0300

    3.16 Отладка программы в Idea

--- src/main/java/ru/condition/DivideBySix.java
+++ src/main/java/ru/condition/DivideBySix.java
@@ -2,17 +2,18 @@ package ru.condition;
 
 public class DivideBySix {
     public static String checkNumber(int number) {
-        String rsl;
-        if (number % 3 == 0 && number % 2 == 0) {
-            rsl = "The number divides by 6.";
-        } else if (number % 3 == 0) {
-            rsl = "The number divides by 3, but it isn't the even number.";
-        } else if (number % 2 == 0) {
-            rsl = "The number doesn't divide by 3, but it is the even number.";
+        boolean divisibleBy3 = number % 3 == 0;
+        boolean even = number % 2 == 0;
+
+        if (even && divisibleBy3) {
+            return "The number divides by 6.";
+        } else if (even) {
+            return "The number doesn't divide by 3, but it is the even number.";
+        } else if (divisibleBy3) {
+            return "The number divides by 3, but it isn't the even number.";
         } else {
-            rsl = "The number doesn't divide by 3 and it isn't the even number.";
+            return "The number doesn't divide by 3 and it isn't the even number.";
         }
-        return rsl;
     }
 }
 
--- src/test/java/ru/condition/DivideBySixTest.java
+++ src/test/java/ru/condition/DivideBySixTest.java
@@ -1,6 +1,7 @@
 package ru.condition;
 
 import org.junit.jupiter.api.Test;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 class DivideBySixTest {
