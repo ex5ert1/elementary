a2e589fdd73765590f915c4115be5b32b99fbfa3
E.Laptev <E.Laptev@amtelsoft.ru>
2025-02-28 11:00:45 +0300

    3.18 Switch - matcher

--- src/main/java/ru/condition/MultipleSwitchWeek.java
+++ src/main/java/ru/condition/MultipleSwitchWeek.java
@@ -5,12 +5,12 @@ public class MultipleSwitchWeek {
         return switch (name) {
             case "Понедельник", "Monday" -> 1;
             case "Вторник", "Tuesday" -> 2;
-                case "Среда", "Wednesday" -> 3;
-                case "Четверг", "Thursday" -> 4;
-                case "Пятница", "Friday" -> 5;
-                case "Суббота", "Saturday" -> 6;
-                case "Воскресенье", "Sunday" -> 7;
-            default  -> -1;
+            case "Среда", "Wednesday" -> 3;
+            case "Четверг", "Thursday" -> 4;
+            case "Пятница", "Friday" -> 5;
+            case "Суббота", "Saturday" -> 6;
+            case "Воскресенье", "Sunday" -> 7;
+            default -> -1;
         };
     }
 
