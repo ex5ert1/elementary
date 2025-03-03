0452a1b15bc5d015f4d2dfad2289ce2ef7c6c39a
E.Laptev <E.Laptev@amtelsoft.ru>
2025-03-03 19:02:12 +0300

    4.10 Ипотека

--- src/main/java/ru/loop/Mortgage.java
+++ src/main/java/ru/loop/Mortgage.java
@@ -3,9 +3,6 @@ package ru.loop;
 public class Mortgage {
     public static int year(double amount, double salary, double percent) {
         int year = 0;
-        if (amount <= 0 || salary <= 0 || percent < 0) {
-            return -1;
-        }
         while (amount > 0) {
             amount += amount * (percent / 100);
             amount -= salary;
