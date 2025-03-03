f4105ea2ab2b55246ed1fe4d5f01185edd00a1c7
E.Laptev <E.Laptev@amtelsoft.ru>
2025-03-03 19:17:59 +0300

    4.8 Простое число, оператор прерывания break

--- src/main/java/ru/loop/CheckPrimeNumber.java
+++ src/main/java/ru/loop/CheckPrimeNumber.java
@@ -2,13 +2,13 @@ package ru.loop;
 
 public class CheckPrimeNumber {
     public static boolean check(int number) {
-        boolean isPrime = true;
+        boolean isPrime = number > 1;
         for (int i = 2; i < number; i++) {
             if (number % i == 0) {
                 isPrime = false;
                 break;
             }
         }
-        return isPrime && number > 1;
+        return isPrime;
     }
 }
