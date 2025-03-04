b7e09a41e978784a5ba1c7b6bd52d2f6366a25eb
E.Laptev <E.Laptev@amtelsoft.ru>
2025-03-04 18:25:05 +0300

    5.3 Заполнение массива

--- src/main/java/ru/array/ArrayDefinition.java
+++ src/main/java/ru/array/ArrayDefinition.java
@@ -8,5 +8,18 @@ public class ArrayDefinition {
         System.out.println("Размер массива равен " + surnames.length);
         float[] prices = new float[40];
         System.out.println("Размер массива равен " + prices.length);
+        String[] names = new String[4];
+        names[0] = "Егор";
+        names[1] = "Антон";
+        names[2] = "Сергей";
+        names[3] = "Федор";
+        String a = names[0];
+        String b = names[1];
+        String c = names[2];
+        String d = names[3];
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
