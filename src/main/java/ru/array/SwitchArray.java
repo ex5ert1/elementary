611875a863fd0381ca71f17c95a4bbcd8940ae4d
E.Laptev <E.Laptev@amtelsoft.ru>
2025-03-05 20:29:33 +0300

    5.11 Переставить элементы массива

--- src/main/java/ru/array/SwitchArray.java
+++ src/main/java/ru/array/SwitchArray.java
@@ -1,6 +1,13 @@
 package ru.array;
 
 public class SwitchArray {
+    public static int[] swap(int[] array, int source, int dest) {
+        int temp = array[source];
+        array[source] = array[dest];
+        array[dest] = temp;
+        return array;
+    }
+
     public static int[] swapBorder(int[] array) {
         int temp = array[0];
         array[0] = array[array.length - 1];
@@ -9,11 +16,12 @@ public class SwitchArray {
     }
 
     public static void main(String[] args) {
-        int[] nums = new int[] {1, 2, 3, 4, 5, 6};
-        int[] rsl = swapBorder(nums);
+        int[] nums = new int[]{1, 2, 3, 4, 5, 6};
+        int[] rsl = swap(nums, 2, 5);
         for (int index = 0; index < rsl.length; index++) {
             System.out.println(rsl[index]);
         }
     }
 }
+
 
