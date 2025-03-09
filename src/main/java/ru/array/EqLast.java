52c5a3d448fa41149c4acff4e8a02af52c8332f8
E.Laptev <E.Laptev@amtelsoft.ru>
2025-03-09 11:46:57 +0300

    5.15 Сравнить последние элементы двух массивов

--- src/main/java/ru/array/EqLast.java
+++ src/main/java/ru/array/EqLast.java
@@ -3,7 +3,7 @@ package ru.array;
 public class EqLast {
     public static boolean check(int[] left, int[] right) {
         for (int i = 0; i < right.length; i++) {
-            if (left[i] == right[i]) {
+            if (left[left.length - 1 - i] == right[right.length - 1 - i]) {
                 return true;
             }
         }
