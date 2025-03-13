de937397fe60455e24e402602f488518d75c2a66
E.Laptev <E.Laptev@amtelsoft.ru>
2025-03-13 19:21:03 +0300

    5.27 Моно строка в матрице

--- src/main/java/ru/array/MatrixCheck.java
+++ src/main/java/ru/array/MatrixCheck.java
@@ -6,6 +6,7 @@ public class MatrixCheck {
         for (int i = 0; i < board[row].length; i++) {
             if (board[row][i] != 'X') {
                 result = false;
+                break;
             }
         }
         return result;
