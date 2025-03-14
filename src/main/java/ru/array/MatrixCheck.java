592ca730d80a1a39627c35821aca88d7644774de
E.Laptev <E.Laptev@amtelsoft.ru>
2025-03-14 17:11:24 +0300

    5.29 массив из диагонали матрицы

--- src/main/java/ru/array/MatrixCheck.java
+++ src/main/java/ru/array/MatrixCheck.java
@@ -20,6 +20,14 @@ public class MatrixCheck {
         }
         return true;
     }
+
+    public static char[] extractDiagonal(char[][] board) {
+        char[] rsl = new char[board.length];
+        for (int i = 0; i < board.length; i++) {
+            rsl[i] = board[i][i];
+        }
+        return rsl;
+    }
 }
 
 
