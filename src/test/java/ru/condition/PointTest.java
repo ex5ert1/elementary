491b3541738f057216e9798ee6009a69d474723f
Egor Laptev <egor.laptev91@yandex.ru>
2025-04-12 15:02:48 +0300

    7.16 Расстояние между точками в трехмерном пространстве

--- src/test/java/ru/condition/PointTest.java
+++ src/test/java/ru/condition/PointTest.java
@@ -10,28 +10,28 @@ class PointTest {
     @Test
     void when00to20then2() {
         double expected = 2;
-        Point x = new Point(0, 0);
-        Point y = new Point(2, 0);
-        double out = x.distance(y);
+        Point p1 = new Point(0, 0);
+        Point p2 = new Point(2, 0);
+        double out = p1.distance(p2);
         assertThat(out).isEqualTo(expected, withPrecision(0.01));
     }
 
     @Test
     void when01to21then2() {
         double expected = 2;
-        Point x = new Point(0, 1);
-        Point y = new Point(2, 1);
-        double out = x.distance(y);
+        Point p1 = new Point(0, 1);
+        Point p2 = new Point(2, 1);
+        double out = p1.distance(p2);
         assertThat(out).isEqualTo(expected, withPrecision(0.01));
     }
 
     @Test
     void when1011to201110then99Dot5037() {
         double expected = 99.5037;
-        Point x = new Point(10, 11);
-        Point y = new Point(20, 110);
+        Point p1 = new Point(10, 11);
+        Point p2 = new Point(20, 110);
         double eps = 0.0001f;
-        double out = x.distance(y);
+        double out = p1.distance(p2);
         assertThat(out).isEqualTo(expected, withPrecision(eps));
     }
 
@@ -47,29 +47,29 @@ class PointTest {
     @Test
     void when00to1010then14Dot1421() {
         double expected = 14.1421;
-        Point x = new Point(0, 0);
-        Point y = new Point(10, 10);
+        Point p1 = new Point(0, 0);
+        Point p2 = new Point(10, 10);
         double eps = 0.0001f;
-        double out = x.distance(y);
+        double out = p1.distance(p2);
         assertThat(out).isEqualTo(expected, withPrecision(eps));
     }
 
     @Test
     void whenMinus34to00then5Dot0() {
         double expected = 5.0;
-        Point x = new Point(-3, 4);
-        Point y = new Point(0, 0);
-        double out = x.distance(y);
+        Point p1 = new Point(-3, 4);
+        Point p2 = new Point(0, 0);
+        double out = p1.distance(p2);
         assertThat(out).isEqualTo(expected, withPrecision(0.01));
     }
 
     @Test
     void when00to1010to55thenCorrectResult() {
-        double expected1 = 14.1421;
-        double expected2 = 7.0710;
-        Point x = new Point(0, 0);
-        Point y = new Point(10, 10);
-        Point z = new Point(5, 5);
+        double expected1 = 17.3205;
+        double expected2 = 8.6602;
+        Point x = new Point(0, 0,0);
+        Point y = new Point(10, 10, 10);
+        Point z = new Point(5, 5,5);
         double eps = 0.0001f;
         double out1 = x.distance3d(y);
         double out2 = x.distance3d(z);
@@ -81,15 +81,15 @@ class PointTest {
 
     @Test
     void whenNegativeCoordinates_thenCorrectDistance3d() {
-        double expected1 = 5;
-        double expected2 = 2.2360;
-        double expected3 = 5.0990;
-        Point x = new Point(-3, 4);
-        Point y = new Point(0, 0);
-        Point z = new Point(-1, 5);
-        double out1 = x.distance3d(y);
-        double out2 = x.distance3d(z);
-        double out3 = y.distance3d(z);
+        double expected1 = 5.3851;
+        double expected2 = 5.4772;
+        double expected3 = 0.0;
+        Point p1 = new Point(-3, 4, 2);
+        Point p2 = new Point(0, 0,0);
+        Point p3 = new Point(-1, 5,2);
+        double out1 = p1.distance3d(p2);
+        double out2 = p2.distance3d(p3);
+        double out3 = p3.distance3d(p3);
         double eps = 0.0001f;
         assertThat(out1).isEqualTo(expected1, withPrecision(eps));
         assertThat(out2).isEqualTo(expected2, withPrecision(eps));
@@ -99,12 +99,12 @@ class PointTest {
     @Test
     void whenSamePoints_thenZeroDistance3d() {
         double expected = 0;
-        Point x = new Point(5, 5);
-        Point y = new Point(5, 5);
-        Point z = new Point(5, 5);
-        double out1 = x.distance3d(y);
-        double out2 = y.distance3d(z);
-        double out3 = z.distance3d(z);
+        Point p1 = new Point(5, 5,5);
+        Point p2 = new Point(5, 5,5);
+        Point p3 = new Point(5, 5,5);
+        double out1 = p1.distance3d(p2);
+        double out2 = p1.distance3d(p3);
+        double out3 = p2.distance3d(p3);
         double eps = 0.0001f;
         assertThat(out1).isEqualTo(expected, withPrecision(eps));
         assertThat(out2).isEqualTo(expected, withPrecision(eps));
